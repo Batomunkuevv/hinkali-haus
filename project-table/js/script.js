c6116d664873981344790d9aace7ea42ed8f5ace
Kirill Batomunkuev <i@batomunkuevv.ru>
2025-06-11 01:47:30 +0900

    Updates

--- project-table/js/script.js
+++ project-table/js/script.js
@@ -20,6 +20,10 @@ const fillTable = () => {
             name: 'Доставка',
             slug: 'delivery.html'
         },
+        {
+            name: 'Политика конфиденциальности',
+            slug: 'policy.html'
+        },
     ]
     const DOMAIN = window.location.href.includes('index.html') ? window.location.href.replace('index.html', '') : window.location.href;
     const projectTable = document.querySelector('.project-table');
